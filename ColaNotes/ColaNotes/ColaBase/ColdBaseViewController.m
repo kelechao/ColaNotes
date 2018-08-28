05f66158eade3f7e50227f511fd3f015f0da8109
可乐超 <18610789532@163.com>
2018-08-28 10:48:58 +0800

    添加启动页和icons

---
+++ ColaNotes/ColaNotes/ColaBase/ColdBaseViewController.m
@@ -0,0 +1,37 @@
+//
+//  ColdBaseViewController.m
+//  ColaNotes
+//
+//  Created by 可乐超 on 2018/8/21.
+//  Copyright © 2018年 可乐超. All rights reserved.
+//
+
+#import "ColdBaseViewController.h"
+
+@interface ColdBaseViewController ()
+
+@end
+
+@implementation ColdBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
